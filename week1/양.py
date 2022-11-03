e8f59e865abbaf614f398badf5db2307f4aca7cc
rnjsdn12332 <82695772+rnjsdn12332@users.noreply.github.com>
2022-11-03 21:49:51 +0900

    Create 양.py

---
+++ week1/양.py
@@ -0,0 +1,10 @@
+def solution(info, edges):
+    answer = 0
+    visit=[False]*len(info)
+    #해야
+    
+    def dfs(graph, x. visit) :
+        visit[x]=True
+        for i in gr
+        if visit[x]==False
+    return answer
